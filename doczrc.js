f9a3a980b597bebbdb240f52514d14f17a08af42
garfieldduck <garfieldduck@gmail.com>
2019-01-25 21:41:01 +0800

    Refine the Playground

--- doczrc.js
+++ doczrc.js
@@ -2,27 +2,31 @@ import * as path from 'path';
 
 const modifyBundlerConfig = config => {
   config.resolve.alias = Object.assign({}, config.resolve.alias, {
-    '@intrasections/core': path.resolve(__dirname, 'packages/core/src'),
+    '@intrasections/core': path.resolve(__dirname, 'packages/core/src')
   });
   return config;
 };
 
 export default {
-  title: 'Intrasections',
-  typescript: true,
-  htmlContext: {
+  title : 'Intrasections',
+  typescript : true,
+  htmlContext : {
     head: {
-      links: [{
-        rel: 'stylesheet',
-        href: 'https://codemirror.net/theme/monokai.css'
-      }]
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'https://codemirror.net/theme/material.css'
+        }
+      ]
     }
   },
   modifyBundlerConfig,
-  themeConfig: {
-    // See: https://github.com/pedronauck/docz/blob/5fbea6a1469d2e258402621ba81f74f898af906a/packages/docz-theme-default/README.md#default-themeconfig
+  themeConfig : {
+    // See:
+    // https://github.com/pedronauck/docz/blob/5fbea6a1469d2e258402621ba81f74f898af90
+    // 6a/packages/docz-theme-default/README.md#default-themeconfig
     mode: 'light',
-    codemirrorTheme: 'monokai',
+    codemirrorTheme: 'material',
     showPlaygroundEditor: true, // always display the code in <Playground>
     colors: {
       primary: '#ff9f00',
@@ -31,15 +35,15 @@ export default {
       blue: '#5211de',
       sidebarBg: '#edeeef',
       sidebarBorder: '#0078e0',
-      border: '0078e0',
+      border: '#0078e0',
       codeBg: '#ffffff',
       codeColor: '#ff9f00',
-      theadColor: '#79878e',
+      theadColor: '#79878e'
     },
     styles: {
       playground: {
-        background: '#ffffff',
-      },
-    },
-  },
+        background: '#ffffff'
+      }
+    }
+  }
 };
