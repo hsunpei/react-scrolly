1f2f61fb5c6276909ca560cc39ab09687dbb3032
Mika Wang <garfieldduck@gmail.com>
2019-02-28 17:31:57 +0800

    Convert the intersection observer as an observable

--- packages/core/src/effects/useIntersectionObservable.ts
+++ packages/core/src/effects/useIntersectionObservable.ts
@@ -1,8 +1,8 @@
 import React, {
-  useState,
   useEffect,
   useRef,
 } from 'react';
+import { Subject } from 'rxjs';
 // TODO: add the polyfill of IntersectionObserver
 
 export interface IntersectionInfo {
@@ -19,15 +19,18 @@ export interface IntersectionInfo {
   sectionBoundingRect: ClientRect;
 }
 
-export function useIntersectionObserver(
+export function useIntersectionObservable(
   sectionRef: React.RefObject<HTMLElement>,
-  onIntersectionUpdated: (inters: IntersectionInfo) => void,
   threshold: number[] | 1 = [0, 0.5, 1],
 ) {
   /**
    * Stores references to the observer listening to section intersection with the viewport
    */
   const intersectionObserverRef = useRef<IntersectionObserver | null>(null);
+
+  // transform the intersectionObserver as a RX Observable
+  const intersectSubjectRef = useRef(new Subject());
+  const intersectObservableRef = useRef(intersectSubjectRef.current.asObservable());
 
   /** Use browser's IntersectionObserver to record whether the section is inside the viewport */
   const recordIntersection = (entries: IntersectionObserverEntry[]) => {
@@ -36,14 +39,16 @@ export function useIntersectionObserver(
 
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const { top, height } = boundingClientRect;
-    const intersecting = {
+    const intersecting: IntersectionInfo = {
       isIntersecting,
       sectionTop: top + scrollTop,
       sectionHeight: height,
       sectionBoundingRect: boundingClientRect,
     };
 
-    onIntersectionUpdated(intersecting);
+    console.log('recordIntersection', intersecting)
+
+    intersectSubjectRef.current.next(intersecting);
   };
 
   useEffect(() => {
@@ -68,9 +73,11 @@ export function useIntersectionObserver(
     return () => {
       if (intersectionObserverRef.current) {
         intersectionObserverRef.current.disconnect();
+        intersectSubjectRef.current.complete();
       }
     };
   },        []);
 
-  return intersectionObserverRef.current;
+  // TODO: return isIntersecting, RxJS stream
+  return intersectObservableRef.current;
 }
