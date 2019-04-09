f2102cc5e9a98e249096f778947d8abf0b1e35e4
garfieldduck <garfieldduck@gmail.com>
2019-04-09 23:42:07 +0800

    Add a simple demo of Plot

--- docs/src/components/DemoSection.jsx
+++ docs/src/components/DemoSection.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { Section } from '@react-scrolly/core';
 
+import { defaultColors } from '../config/theme';
+
 export const DemoSection = styled(Section)`
   position: relative;
   height: ${({ height = '100vh' }) => height};
@@ -8,4 +10,17 @@ export const DemoSection = styled(Section)`
   box-shadow: 0 15px 30px 0 rgba(0,0,0,.11), 0 6px 16px 0 rgba(0,0,0,.08);
   border-radius: 2px;
   margin: 1rem 0;
+`;
+
+export const BorderedDemoSection = styled(Section)`
+  position: relative;
+  margin-top: -3px;
+  height: ${({ height = '100vh' }) => height};
+  border: 3px solid ${defaultColors.background};
+  border-radius: 2px;
+
+  h5 {
+    margin-top: 0.5rem;
+    margin-left: 0.7rem;
+  }
 `;
