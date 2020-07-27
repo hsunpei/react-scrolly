decd8e74f2918bcbfa6c7fbbf5dc8f3a39ba0591
HsunPei Wang <garfieldduck@gmail.com>
2020-07-27 20:26:55 +0800

    Add back all the documents

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -1,8 +1,21 @@
 export default {
+  title : 'React-Scrolly',
+  repository: 'https://github.com/garfieldduck/react-scrolly',
+
   docgenConfig: {
     searchPath: '../packages',
   },
 
+  port: 5000,
+
   // Ignore README.md
   files: '**/*.mdx',
+
+  // order of the menu
+  menu: [
+    'Introduction',
+    'Scroll Tracking',
+    'Pinning Sections',
+    'Revealing Animations'
+  ],
 };
