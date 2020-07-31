5cd0e496bc2417c44f234a9b654f606051dde351
HsunPei Wang <garfieldduck@gmail.com>
2020-08-01 00:10:11 +0800

    Refine the box-shadow of the DemoWrapper for the dark theme

--- docs/src/components/DemoWrapper.jsx
+++ docs/src/components/DemoWrapper.jsx
@@ -4,5 +4,5 @@ export const DemoWrapper = styledComponents.div`
   padding: 2rem 1rem;
   margin: 1.8rem 0;
   border-radius: 5px;
-  box-shadow: 0 12px 22px 0 rgba(0,0,0,.06), 0 6px 14px 0 rgba(0,0,0,.03);
+  box-shadow: 0 12px 22px 0 rgba(180,180,180,.3), 0 6px 14px 0 rgba(180,180,180,.3);
 `;
