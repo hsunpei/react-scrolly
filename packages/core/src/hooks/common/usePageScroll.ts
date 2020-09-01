64284ea2943f74722b60d595cea49d8aaf6746ae
HsunPei Wang <garfieldduck@gmail.com>
2020-09-01 18:44:48 +0200

    Refactor usePageScroll using observable-hooks

--- packages/core/src/hooks/common/usePageScroll.ts
+++ packages/core/src/hooks/common/usePageScroll.ts
@@ -1,103 +1,89 @@
 import { Observable, of, zip } from 'rxjs';
-import { map, switchMap, merge, take } from 'rxjs/operators';
-import { useState, useEffect, useRef, useContext } from 'react';
+import { map, switchMap, merge, take, filter } from 'rxjs/operators';
+import { useCallback, useContext, useMemo } from 'react';
+import { useObservableState } from 'observable-hooks';
 
 import { PageContext, PageContextInterface } from '../../context/PageContext';
-
 import { IntersectionInfo } from './useIntersectionObservable';
-import { useSubscription } from './useSubscription';
 import { ScrollPosition } from '../../types/ScrollPosition';
 
 export function usePageScroll(intersectObsr$: Observable<IntersectionInfo>) {
   const context = useContext<PageContextInterface | null>(PageContext);
   const { scrollObs$ } = context!;
 
-  const [intersecting, setIntersecting] = useState<boolean>(false);
-
-  const [scrollInfo, setScrollInfo] = useState<ScrollPosition>({
-    scrollTop: 0,
-    scrollBottom: 0,
-    windowHeight: 0,
-    scrollOffset: 0,
-  });
-
-  /** Function to set the subscription to the page scrolling  */
-  const { setSubscription: setPageSubscpt } = useSubscription(null);
-  const isIntersectingObs = useRef(
-    intersectObsr$.pipe(map(({ isIntersecting }) => isIntersecting))
+  const isIntersectingObs = useMemo(
+    () => intersectObsr$.pipe(map(({ isIntersecting }) => isIntersecting)),
+    [intersectObsr$]
   );
+  const isInterSectingFunc = useCallback(() => isIntersectingObs, [isIntersectingObs]);
+  const [intersecting] = useObservableState(isInterSectingFunc, false);
 
   /**
    * Observer to track the scroll position
    * emitted when Page is mounted
    */
-  const mountScrollObsRef = useRef(
-    zip(scrollObs$, isIntersectingObs.current).pipe(
-      map(([scrollPos, isIntersecting]): {
-        isIntersecting: boolean;
-        scrollPos: ScrollPosition;
-      } => ({
-        isIntersecting,
-        scrollPos: {
-          ...scrollPos,
-          scrollOffset: 0,
-        },
-      })),
-      take(1)
-    )
+  const mountScrollObs = useMemo(
+    () =>
+      zip(scrollObs$, isIntersectingObs).pipe(
+        map(([scrollPos, isIntersecting]): {
+          isIntersecting: boolean;
+          scrollPos: ScrollPosition;
+        } => ({
+          isIntersecting,
+          scrollPos: {
+            ...scrollPos,
+            scrollOffset: 0,
+          },
+        })),
+        take(1)
+      ),
+    [isIntersectingObs, scrollObs$]
   );
   /**
    * Observer to track the scroll position
    * when real scrolling events are triggered
    */
-  const windowScrollObsRef = useRef(
-    isIntersectingObs.current.pipe(
-      switchMap((isIntersecting: boolean) => {
-        // use `isIntersecting` to determine whether to take the scrolling info
-        return isIntersecting
-          ? scrollObs$.pipe(
-              map((scrollPos: ScrollPosition) => ({
+  const windowScrollObs = useMemo(
+    () =>
+      isIntersectingObs.pipe(
+        switchMap((isIntersecting: boolean) => {
+          // use `isIntersecting` to determine whether to take the scrolling info
+          return isIntersecting
+            ? scrollObs$.pipe(
+                map((scrollPos: ScrollPosition) => ({
+                  isIntersecting,
+                  scrollPos,
+                }))
+              )
+            : // when the section is scrolled out of the viewport, update its dimension
+              of({
                 isIntersecting,
-                scrollPos,
-              }))
-            )
-          : // when the section is scrolled out of the viewport, update its dimension
-            of({
-              isIntersecting,
-              scrollPos: null,
-            });
-      })
-    )
+                scrollPos: null,
+              });
+        })
+      ),
+    [isIntersectingObs, scrollObs$]
   );
+
   /** Observer to track the page scrolling by combining mountScrollObs and windowScrollObs */
-  const pageScrollObsrRef = useRef(
-    mountScrollObsRef.current.pipe(merge(windowScrollObsRef.current))
+  const pageScrollObsrFunc = useCallback(
+    () =>
+      mountScrollObs.pipe(
+        merge(windowScrollObs),
+        filter(({ isIntersecting, scrollPos }) => isIntersecting && scrollPos !== null),
+        map(({ scrollPos }) => {
+          return scrollPos!;
+        })
+      ),
+    [mountScrollObs, windowScrollObs]
   );
 
-  useEffect(() => {
-    // subscribe to the page scrolling
-    setPageSubscpt(
-      pageScrollObsrRef.current.subscribe({
-        // record the page scrolling
-        next: ({ isIntersecting, scrollPos }) => {
-          if (isIntersecting && scrollPos) {
-            const { scrollTop, scrollBottom, windowHeight, scrollOffset } = scrollPos;
-
-            // updates the ratio of the section being scrolled and the scroll positions
-            setScrollInfo({
-              scrollTop,
-              scrollBottom,
-              windowHeight,
-              scrollOffset,
-            });
-          }
-
-          // update the intersecting state
-          setIntersecting(isIntersecting);
-        },
-      })
-    );
-  }, [setPageSubscpt]);
+  const [scrollInfo] = useObservableState<ScrollPosition>(pageScrollObsrFunc, {
+    scrollTop: 0,
+    scrollBottom: 0,
+    windowHeight: 0,
+    scrollOffset: 0,
+  });
 
   return {
     scrollInfo,
