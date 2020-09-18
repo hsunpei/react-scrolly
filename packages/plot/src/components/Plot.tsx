14e249b88084ce00f20a516cdfc2c8e13c6ba941
HsunPei Wang <garfieldduck@gmail.com>
2020-09-18 20:20:27 +0200

    Refine the typings of the plots

--- packages/plot/src/components/Plot.tsx
+++ packages/plot/src/components/Plot.tsx
@@ -2,11 +2,20 @@ import React, { useRef } from 'react';
 import {
   // types
   SectionProps,
+  SectionInfo,
+  ActiveSectionInfo,
 } from '@react-scrolly/core';
 
 import { usePlot } from '../hooks/usePlot';
 
-export const Plot = ({ className, style, children, trackingId, ...restProps }: SectionProps) => {
+export type PlotRenderProps = (
+  section: SectionInfo & { activeSection: ActiveSectionInfo | null | undefined }
+) => React.ReactNode;
+export interface PlotProps extends Omit<SectionProps, 'children'> {
+  children: PlotRenderProps;
+}
+
+export const Plot = ({ className, style, children, trackingId, ...restProps }: PlotProps) => {
   const plotRef = useRef<HTMLDivElement>(null);
   const { sectionInfo, activeSection } = usePlot(plotRef, trackingId);
 
